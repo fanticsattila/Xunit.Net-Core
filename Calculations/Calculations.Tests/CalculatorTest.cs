a4484ee1710d45435b3c73b69f895495138f062e
Fántics Attila <fantics.attila@windowslive.com>
2018-12-09 20:03:59 +0100

    Számok összehasonlítása - sikeres teszt futtatás

--- Calculations/Calculations.Tests/CalculatorTest.cs
+++ Calculations/Calculations.Tests/CalculatorTest.cs
@@ -5,9 +5,19 @@ namespace Calculations.Tests
     public class CalculatorTest
     {
         [Fact]
-        public void TestAdd()
+        public void Add_GivenTwoIntValues_ReturnsInt()
         {
-            Assert.True(true);
+            var calc = new Calculator();
+            var result = calc.Add(1, 2);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void AddDouble_GivenTwoDoubleValues_ReturnsDouble()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.21, 3.56);
+            Assert.Equal(4.8, result, 1);
         }
     }
 }
