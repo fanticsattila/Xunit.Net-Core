aefe98c5fb74ad2cd34d713665d159b2cadff30f
Fántics Attila <fantics.attila@windowslive.com>
2018-12-09 19:46:05 +0100

    Első teszt projektem. - Add / TestAdd

---
+++ Calculations/Calculations.Tests/CalculatorTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Calculations.Tests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
