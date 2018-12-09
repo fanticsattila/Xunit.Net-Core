639a78d453b180519b97c43002f73006a5c00c98
Fántics Attila <fantics.attila@windowslive.com>
2018-12-09 20:17:41 +0100

    Szöveg tartalmazza összehasonlítása - sikeres tesztelés

--- Calculations/Calculations.Tests/NamesTest.cs
+++ Calculations/Calculations.Tests/NamesTest.cs
@@ -9,7 +9,7 @@ namespace Calculations.Tests
         {
             var names = new Names();
             var result = names.MakeFullName("fántics", "Attila");
-            Assert.Equal("Fántics Attila", result, ignoreCase: true);
+            Assert.Contains("Attila", result);
         }
     }
 }
