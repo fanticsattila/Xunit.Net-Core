2671bbf5f607503eec43a6b710848cec23cc64a3
Fántics Attila <fantics.attila@windowslive.com>
2018-12-10 19:50:33 +0100

    Érték adott tartományon belül van-e

---
+++ Calculations/Calculations.Tests/CustomerTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Calculations.Tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void CheckNameNotEmpty()
+        {
+            var customer = new Customer();
+            Assert.NotNull(customer.Name);
+            Assert.False(string.IsNullOrEmpty(customer.Name));
+        }
+
+        [Fact]
+        public void CheckLegitForDiscount()
+        {
+            var customer = new Customer();
+            Assert.InRange(customer.Age, 35, 45);
+        }
+    }
+}
---
+++ Calculations/Calculations/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculations
+{
+    public class Customer
+    {
+        public string Name => "Fántics Attila";
+        public int Age => 41;
+    }
+}
