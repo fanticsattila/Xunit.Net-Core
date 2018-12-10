ca03d5bc2ca3208e81a4643f25bd6897949be1ab
Fántics Attila <fantics.attila@windowslive.com>
2018-12-10 19:35:20 +0100

    List nem tartalmaz adott értéket (0-t)

---
+++ Calculations/Calculations.Tests/CalculationsTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Calculations.Tests
+{
+    public class CalculationsTest
+    {
+        [Fact]
+        public void FiboDoesNotIncludeZero()
+        {
+            var calc = new Calculations();
+            Assert.All(calc.FiboNumbers, n => Assert.NotEqual(0, n));
+        }
+    }
+}
---
+++ Calculations/Calculations/Calculations.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Calculations
+{
+    public class Calculations
+    {
+        public List<int> FiboNumbers => new List<int> { 1, 1, 2, 3, 5, 8, 13 };
+    }
+}
