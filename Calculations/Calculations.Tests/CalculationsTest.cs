16c9b96def4a6c1fa9c3adc3c5c470f78b81c1a9
Fántics Attila <fantics.attila@windowslive.com>
2018-12-16 18:54:55 +0100

    Data Driven Test - Theory, MemberData  - külső adatforrásból

--- Calculations/Calculations.Tests/CalculationsTest.cs
+++ Calculations/Calculations.Tests/CalculationsTest.cs
@@ -84,7 +84,7 @@ namespace Calculations.Tests
         }
 
         [Theory]
-        [MemberData(nameof(TestDataShare.IsOddOrEvenData), MemberType = typeof(TestDataShare))]
+        [MemberData(nameof(TestDataShare.IsOddOrEvenExternalData), MemberType = typeof(TestDataShare))]
         public void IsOdd_TestOddAndEven(int value, bool expected)
         {
             var calc = _calculatorFixture.Calc;
--- Calculations/Calculations.Tests/TestDataShare.cs
+++ Calculations/Calculations.Tests/TestDataShare.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Calculations.Tests
 {
@@ -10,6 +11,19 @@ namespace Calculations.Tests
             {
                 yield return new object[] { 1, true };
                 yield return new object[] { 200, false };
+            }
+        }
+
+        public static IEnumerable<object[]> IsOddOrEvenExternalData
+        {
+            get
+            {
+                var allLines = System.IO.File.ReadAllLines("IsOddOrEvenTestData.txt");
+                return allLines.Select(x =>
+                {
+                    var lineSplit = x.Split(',');
+                    return new object[] { int.Parse(lineSplit[0]), bool.Parse(lineSplit[1]) };
+                });
             }
         }
     }
